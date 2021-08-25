342b8db8f480957f74e9f294b494df48797e0161
Mparker93 <marcuseparker@hotmail.com>
2021-08-25 02:03:32 +0100

    Adding boundaries, rotation and movement. Needs a lot of improvement

--- tetris/try_things_out.py
+++ tetris/try_things_out.py
@@ -1,6 +1,7 @@
 import pyxel
 import random
 import constants
+import pprint
 
 FALL_SPEED = 10
 
@@ -13,15 +14,17 @@ class Tetris:
         pyxel.run(self.update, self.draw)
 
     def reset(self):
-        self.piece = Pieces()
-        self.board = Board()
         self.level = 0
         self.score = 0
         self.lines = 0
         self.combos = 0
+        self.piece = Pieces()
+        self.board = Board()
+        self.current_piece = self.piece.generate_block()
         self.is_gameover = False
         self.game_state = "running"
         self.direction = None
+        self.rotation = None
 
     def update(self):
         if pyxel.btn(pyxel.KEY_Q) or self.is_gameover == True:
@@ -32,7 +35,7 @@ class Tetris:
                 self.game_state = "paused"
             else:
                 self.game_state = "running"
-
+        
         if self.game_state == "running":
             if pyxel.btn(pyxel.KEY_R):
                 self.reset()
@@ -42,35 +45,62 @@ class Tetris:
 
             if pyxel.btnp(pyxel.KEY_DOWN, 10, 2):
                 self.direction = "DOWN"
-                self.piece.move_block(self.direction)
+                self.move_block(self, self.direction)
 
             elif pyxel.btnp(pyxel.KEY_LEFT, 10, 2):
                 self.direction = "LEFT"
-                self.piece.move_block(self.direction)
+                self.move_block(self, self.direction)
 
             elif pyxel.btnp(pyxel.KEY_RIGHT, 10, 2):
                 self.direction = "RIGHT"
-                self.piece.move_block(self.direction)
+                self.move_block(self, self.direction)
 
             elif pyxel.btnp(pyxel.KEY_SPACE, 10, 2):
                 self.direction = "SPACE"
-                self.piece.move_block(self.direction)
+                self.move_block(self, self.direction)
 
             elif pyxel.btnp(pyxel.KEY_Z, 10, 2):
-                self.direction = "R_LEFT"
+                self.rotation = "ACW"
+                self.current_piece = self.rotate_anticlockwise(self)
 
             elif pyxel.btnp(pyxel.KEY_X, 10, 2):
-                self.direction = "R_RIGHT"
+                self.rotation = "CW"
+                self.current_piece = self.rotate_clockwise(self)
 
     def draw(self):
         pyxel.cls(0)
         self.board.draw_border()
         self.board.draw_block()
         self.piece.draw_block()
+        self.add_block_to_board()
         self.Text.show_text(self)
 
         if self.game_state == "paused":
             pyxel.text(50, 100, "PAUSED", pyxel.frame_count % 16)
+            
+    def rotate_clockwise(self):
+        self.current_piece = list(zip(*self.current_piece[::-1]))
+        return self.current_piece
+        
+    def rotate_anticlockwise(self):
+        rotate_once = self.rotate_clockwise(self.current_piece)
+        rotate_twice = self.rotate_clockwise(rotate_once)
+        self.current_piece = self.rotate_clockwise(rotate_twice)
+        return self.current_piece
+
+    def move_block(self, direction):
+        pass    
+    
+    def check_for_collisions(self):
+        for row in range(22):
+            for col in range(10):
+                pass
+    def add_block_to_board(self):
+        pass
+        # for row in range(22):
+        #     for col in range(10):
+        #         if self.piece.board[row][col] != 0:
+        #             self.piece.board[row][col] = self.board.board[row][col]
 
     class Text:
         def __init__(self):
@@ -97,10 +127,10 @@ class Tetris:
 class Board:
     def __init__(self):
         self.board = [[0] * 10 for _ in range(22)]
-
+        
     def draw_border(self):
         # Main area for gameplay
-        pyxel.rectb(4, 8, 10 * 8, 22 * 8, 5)
+        pyxel.rectb(4, 8, 10 * 8, 22 * 7.8, 5)
 
         # Show next block
         pyxel.rectb(87, 16, 30, 48, 5)
@@ -116,7 +146,6 @@ class Board:
 class Pieces(Board):
     def __init__(self):
         super().__init__()
-        self.generate_block()
 
     def block_falling(self):
         for row in reversed(range(0, 22)):
@@ -125,116 +154,66 @@ class Pieces(Board):
         self.board[0] = [0 for x in range(10)]
 
     def generate_block(self):
-        block = random.choice(
-            [val for val in constants.PYXRES_VALUES.values()])
+        block = random.choice([_ for _ in range(7)])
 
-        if block == 8:  # O Block
-            self.board[0][5] = 8
-            self.board[0][4] = 8
-            self.board[1][5] = 8
-            self.board[1][4] = 8
+        blocks = [
+            # O block
+            [
+                [8, 8],
+                [8, 8]
+            ],
 
-        elif block == 24:  # L Block
-            self.board[0][5] = 24
-            self.board[1][5] = 24
-            self.board[2][5] = 24
-            self.board[2][6] = 24
+            # I block
+            [
+                [0, 0, 0, 0],
+                [88, 88, 88, 88],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ],
 
-        elif block == 72:  # J Block
-            self.board[0][6] = 72
-            self.board[0][5] = 72
-            self.board[1][5] = 72
-            self.board[2][5] = 72
+            # S block
+            [
+                [0, 192, 192],
+                [192, 192, 0],
+                [0, 0, 0],
+            ],
 
-        elif block == 88:  # I Block
-            self.board[0][5] = 88
-            self.board[1][5] = 88
-            self.board[2][5] = 88
-            self.board[3][5] = 88
+            # Z block
+            [
+                [160, 160, 0],
+                [0, 160, 160],
+                [0, 0, 0]
+            ],
 
-        elif block == 128:  # T Block
-            self.board[0][5] = 128
-            self.board[1][5] = 128
-            self.board[1][6] = 128
-            self.board[2][5] = 128
+            # L block
+            [
+                [0, 24, 0],
+                [0, 24, 0],
+                [0, 24, 24],
+            ],
 
-        elif block == 160:  # Z Block
-            self.board[0][5] = 160
-            self.board[1][5] = 160
-            self.board[1][4] = 160
-            self.board[2][4] = 160
+            # J block
+            [
+                [0, 72, 0],
+                [0, 72, 0],
+                [72, 72, 0],
+            ],
 
-        else:  # S Block
-            self.board[0][5] = 192
-            self.board[1][5] = 192
-            self.board[1][6] = 192
-            self.board[2][6] = 192
+            # T block
+            [
+                [0, 128, 0],
+                [128, 128, 128],
+                [0, 0, 0]
+            ]
+        ]
 
-    def move_block(self, direction):
-        for row in range(len(self.board)):
-            for col in range(len(self.board[row])):
-                if self.board[row][col] != 0:
-                    if direction == "RIGHT":
-                        self.board[row][col] = self.board[row][col + 1]
+        return blocks[block]
 
-                    if direction == "LEFT":
-                        self.board[row][col] = self.board[row][col - 1]
-
-                    if direction == "DOWN":
-                        self.board[row][col] = self.board[col + 1]
-
-        # blocks = [
-        #     # O block
-        #     [
-        #         [8, 8],
-        #         [8, 8]
-        #     ],
-
-        #     # I block
-        #     [
-        #         [0, 0, 0, 0],
-        #         [88, 88, 88, 88],
-        #         [0, 0, 0, 0],
-        #         [0, 0, 0, 0]
-        #     ],
-
-        #     # S block
-        #     [
-        #         [0, 192, 192],
-        #         [192, 192, 0],
-        #         [0, 0, 0],
-        #     ],
-
-        #     # Z block
-        #     [
-        #         [160, 160, 0],
-        #         [0, 160, 160],
-        #         [0, 0, 0]
-        #     ],
-
-        #     # L block
-        #     [
-        #         [24, 0, 0],
-        #         [24, 0, 0],
-        #         [24, 24, 0],
-        #     ],
-
-        #     # J block
-        #     [
-        #         [0, 72, 0],
-        #         [0, 72, 0],
-        #         [72, 72, 0],
-        #     ],
-
-        #     # T block
-        #     [
-        #         [0, 128, 0],
-        #         [128, 128, 128],
-        #         [0, 0, 0]
-        #     ]
-        # ]
-
-        # return blocks[block]
+    def insert_block_in_board(self, block):
+        for section in block:
+            for i, elem in enumerate(section):
+                self.board[i].pop()
+                self.board[i].insert(3, elem)
 
 
 if __name__ == "__main__":
